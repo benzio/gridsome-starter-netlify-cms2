95c137b04a3fc0c2f7102b64d06c0c436d362b08
lars.klopstra@beeproger.com <larsklopstra@gmail.com>
2019-03-22 13:40:23 +0100

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <Layout>
+    <h1>Welcome to my blog :)</h1>
+    <Pager :info="$page.allPost.pageInfo"/>
+    <ul>
+      <li v-for="{ node } in $page.allPost.edges" :key="node._id">
+        <h2 v-html="node.title"/>
+        <div v-html="node.excerpt"/>
+        <router-link :to="node.path">Read more</router-link>
+      </li>
+    </ul>
+  </Layout>
+</template>
+
+<page-query>
+query Home ($page: Int) {
+  allPost (perPage: 10, page: $page) @paginate {
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        _id
+        title
+        path
+        excerpt
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import { Pager } from 'gridsome'
+
+export default {
+  components: {
+    Pager
+  }
+}
+</script>
